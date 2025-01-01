ab376d06002a3b53d274dcb0b4a09fc03e26b71e
guorong wu <daniel.stillpixels@gmail.com>
2025-01-01 16:23:49 +0000

    display all customers

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const hbs = require('hbs');
-const wax = require('wax-on');
+const waxOn = require('wax-on');
 require('dotenv').config();
 const { createConnection } = require('mysql2/promise');
-const waxOn = require('wax-on');
+
 
 let app = express();
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 waxOn.on(hbs.handlebars);
 waxOn.setLayoutPath('./views/layouts');
+
+// Include the 188 handlebar helpers
+const helpers = require('handlebars-helpers')({
+    handlebars: hbs.handlebars
+});
+
 
 let connection;
 
@@ -23,14 +29,27 @@ async function main() {
         'password': process.env.DB_PASSWORD
     })
 
-app.get('/', (req,res) => {
-    res.redirect('/base');
-   // res.send('Hello, World!');
+    app.get('/', async function (req, res) {
+        res.render('index.hbs');
 
-});
+    });
 
-}main();
+    app.get('/customers', async function (req, res) {
+        let [customers] = await connection.execute('SELECT * FROM Customers Inner JOIN Pnotes ON Customers.cust_id = Pnotes.cust_id');
 
-app.listen(3000, ()=>{
+        // const [customers] = await connection.execute({
+        //     'sql': query,
+        //     'nestTables': true
+        // }, bindings);
+
+        res.render('customers', {
+            "allCustomers": customers,
+        })
+    })
+
+}
+main();
+
+app.listen(3000, () => {
     console.log('Server is running')
 });
