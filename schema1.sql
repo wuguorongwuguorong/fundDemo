678c0cb7187377c8a5e104dae0e08635630ddc17
guorong wu <daniel.stillpixels@gmail.com>
2025-01-08 12:23:06 +0000

    change of datebase

--- schema1.sql
+++ schema1.sql
@@ -1,15 +1,12 @@
+CREATE DATABASE mgmtFundOverview;
 
-CREATE DATABASE fundOverview;
+USE mgmtFundOverview;
 
-USE fundOverview;
-
---Creating customer table
 create table Customers (
     cust_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    maintenance_fee DECIMAL(10,2) NOT NULL,
     cFirst_name VARCHAR(255) NOT NULL,
     cLast_name VARCHAR(255) NOT NULL,
-    dob DATETIME NOT NULL,
+    dob DATE NOT NULL,
     NRIC VARCHAR(16) NOT NULL,
     gender CHAR NOT NULL,
     addr_1 VARCHAR(45) NOT NULL,
@@ -38,14 +35,16 @@ CREATE TABLE Banks(
 
 CREATE TABLE Pnotes(
     pnote_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    pstart_date DATETIME NOT NULL,
-    pend_date DATETIME,
-    invest_amt DECIMAL(10,2) NOT NULL
+    pstart_date DATE NOT NULL,
+    pend_date DATE,
+    invest_amt DECIMAL(10,2) NOT NULL,
+    maintenance_fee DECIMAL(10,2) NOT NULL
 );
+
 
 CREATE TABLE Dividends(
     divi_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    payout_date DATETIME,
+    payout_date DATE,
     nav_payout DECIMAL(10,2) NOT NULL
 );
 
@@ -54,16 +53,17 @@ CREATE TABLE MonthlyComms(
     comms_base DECIMAL (10,2) NOT NULL
 );
 
-alter table Customers ADD Column pnote_id int UNSIGNED;
-alter table Customers add constraint fk_customer_pnote FOREIGN KEY (pnote_id) REFERENCES Pnotes(pnote_id);
-
 
 alter table Pnotes add column cust_id INT UNSIGNED; 
 alter table Pnotes add column bank_id INT UNSIGNED;
 alter table Pnotes add column divi_id int UNSIGNED;
+alter table Pnotes add column agent_id int UNSIGNED;
 alter table Pnotes add constraint fk_pnote_customer FOREIGN KEY(cust_id) REFERENCES Customers(cust_id);
 alter table Pnotes add constraint fk_pnote_bank FOREIGN KEY (bank_id) REFERENCES Banks(bank_id);
 alter table Pnotes add constraint fk_pnote_dividend FOREIGN KEY(divi_id)REFERENCES Dividends(divi_id);
+alter table Pnotes add constraint fk_pnote_agent FOREIGN KEY(agent_id)REFERENCES Agents(agent_id);
+
+
 
 alter table MonthlyComms add column pnote_id INT UNSIGNED;
 alter table MonthlyComms add column agent_id INT UNSIGNED;
